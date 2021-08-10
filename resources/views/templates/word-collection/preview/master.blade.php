7172c825b75ed2b7a45e3953011a8f5db12e525a
Sergey Bruhin <sundaycreative@gmail.com>
2021-08-11 00:53:25 +0300

    Update collections preview template

--- resources/views/templates/word-collection/preview/master.blade.php
+++ resources/views/templates/word-collection/preview/master.blade.php
@@ -1,6 +1,17 @@
-<a href="{{ route('wordCollections.show', $wordCollection->id) }}"  class="card bg-dark text-white">
-    <img src="{{ $wordCollection->image }}" class="card-img" alt="{{ Str::upper($wordCollection->name) }}">
-    <div class="card-img-overlay">
-        <h5 class="card-title my-4">{{ Str::upper($wordCollection->name) }}</h5>
+{{--<a href="{{ route('wordCollections.show', $wordCollection->id) }}" class="card bg-dark text-white">--}}
+{{--    <img src="{{ $wordCollection->image }}" class="card-img" alt="{{ Str::upper($wordCollection->name) }}">--}}
+{{--    <div class="card-img-overlay">--}}
+{{--        <h5 class="card-title my-4">{{ Str::upper($wordCollection->name) }}</h5>--}}
+{{--    </div>--}}
+{{--</a>--}}
+
+<a href="{{ route('wordCollections.show', $wordCollection->id) }}" class="t-wordCollectionPreview">
+    <div class="t-wordCollectionPreview__container">
+        @forelse($wordCollection->words->take(4) as $word)
+            <img src="{{ $word->image }}" class="t-wordCollectionPreview__image" alt="{{ Str::upper($wordCollection->name) }}">
+        @empty
+        @endforelse
     </div>
+
+    <h5 class="t-wordCollectionPreview__title">{{ Str::upper($wordCollection->name) }}</h5>
 </a>
