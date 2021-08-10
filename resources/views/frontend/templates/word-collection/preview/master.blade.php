75770ac7b91e133827964cab27124fcf641498a3
Sergey Bruhin <sundaycreative@gmail.com>
2021-08-11 02:18:55 +0300

    Add words count to collection card

--- resources/views/frontend/templates/word-collection/preview/master.blade.php
+++ resources/views/frontend/templates/word-collection/preview/master.blade.php
@@ -8,10 +8,12 @@
 <a href="{{ route('wordCollections.show', $wordCollection->id) }}" class="t-wordCollectionPreview">
     <div class="t-wordCollectionPreview__container">
         @forelse($wordCollection->words->take(4) as $word)
-            <img src="{{ $word->image }}" class="t-wordCollectionPreview__image" alt="{{ Str::upper($wordCollection->name) }}">
+            <img src="{{ $word->image }}" class="t-wordCollectionPreview__image"
+                 alt="{{ Str::upper($wordCollection->name) }}">
         @empty
         @endforelse
     </div>
 
     <h5 class="t-wordCollectionPreview__title">{{ Str::upper($wordCollection->name) }}</h5>
+    <span class="t-wordCollectionPreview__wordCount">Слов: {{ $wordCollection->words->count() }}</span>
 </a>
