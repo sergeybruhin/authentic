2e12aa3c6e9658efc14beb144184fff7c1c30a3a
Sergey Bruhin <sundaycreative@gmail.com>
2021-08-27 20:27:33 +0300

    Add sounds

--- resources/views/frontend/pages/word/master.blade.php
+++ resources/views/frontend/pages/word/master.blade.php
@@ -1,14 +1,14 @@
 @extends('frontend.layouts.default.master')
 @section('content')
     @include('frontend.widgets.success-modal.master')
-    <div class="container pt-5 pb-4" >
-        <div class="row " style="display: flex; flex-wrap: wrap;" >
+    <div class="container pt-5 pb-4">
+        <div class="row " style="display: flex; flex-wrap: wrap;">
             <div class="col-lg-6">
                 @include('frontend.widgets.editor.master')
             </div>
             <div class="col-10 col-sm-8 col-lg-6">
                 <div class="" style="max-height: 78vh;height: 100%">
-                    <img class="img-fluid" src="{{ $word->image }}" alt="" style="max-height: 78vh" >
+                    <img class="img-fluid" src="{{ $word->image }}" alt="" style="max-height: 78vh">
                     <h4 class="text-center my-3">{{ Str::upper($word->text) }}</h4>
                 </div>
 
@@ -19,18 +19,21 @@
         <div class="row">
             <div class="col">
                 @if($previousWord)
-                    <a id="prevButton" class="btn btn-primary btn-lg text-uppercase px-4" style="display: none;border-radius: 20px"
+                    <a id="prevButton" class="btn btn-primary btn-lg text-uppercase px-4"
+                       style="display: none;border-radius: 20px"
                        href="{{ route('words.show', $previousWord->id) }}">👈 <span class="mx-2"></span> Назад</a>
                 @endif
             </div>
             <div class="col text-end">
                 @if($nextWord)
                     @if(isset($wordCollection))
-                        <a id="nextButton" class="btn btn-primary btn-lg text-uppercase px-4" style="display: none;border-radius: 20px"
+                        <a id="nextButton" class="btn btn-primary btn-lg text-uppercase px-4"
+                           style="display: none;border-radius: 20px"
                            href="{{ route('wordCollections.showWord', [$wordCollection->id, $nextWord->id]) }}">Вперёд
                             👉</a>
                     @else
-                        <a id="nextButton" class="btn btn-primary btn-lg text-uppercase px-4" style="display: none;border-radius: 20px"
+                        <a id="nextButton" class="btn btn-primary btn-lg text-uppercase px-4"
+                           style="display: none;border-radius: 20px"
                            href="{{ route('words.show', $nextWord->id) }}">Вперёд 👉</a>
                     @endif
                 @endif
@@ -46,16 +49,30 @@
             </div>
         @endif
     </div>
+    <audio id="successWordSound" src="/storage/success-word.mp3" preload="auto"></audio>
+    <audio id="successCollectionSound" src="/storage/success-collection.wav" preload="auto"></audio>
+
 @endsection
 
 @section('scripts')
     <script>
+        successWordSound = document.getElementById('successWordSound');
+        successCollectionSound = document.getElementById('successCollectionSound');
+
+        const playSuccessWord = function () {
+            successWordSound.play();
+        };
+        const playSuccessCollection = function () {
+            successCollectionSound.play();
+        }
+
         const word = '{{ Str::upper($word->text) }}';
         const editor = document.getElementById('editor');
         const nextButton = document.getElementById('nextButton');
         const prevButton = document.getElementById('prevButton');
         const successModal = new bootstrap.Modal(document.getElementById('successModal'))
         document.addEventListener('keyup', logKey);
+
         function logKey(e) {
             if (editor.textContent.toLowerCase() === word.toLowerCase()) {
                 if (!editor.classList.contains('editor--success')) {
@@ -63,15 +80,18 @@
                     editor.contentEditable = false;
                     if (nextButton) {
                         nextButton.style.display = 'inline-block';
+                        playSuccessWord();
                     }
                     if (!nextButton) {
                         successModal.show();
+                        playSuccessCollection();
                     }
+
                 }
             }
             if (editor.textContent.toLowerCase() !== word.toLowerCase()) {
                 if (editor.classList.contains('editor--success')) {
-                    editor.classList.remove('editor--success')
+                    editor.classList.remove('editor--success');
                 }
             }
             console.log(editor.textContent);
