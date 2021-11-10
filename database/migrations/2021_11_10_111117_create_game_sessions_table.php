5ae7ad152479d7389f6cd7a13ef803be0e887b36
Sergey Bruhin <sundaycreative@gmail.com>
2021-11-10 11:29:24 +0300

    Add game session

---
+++ database/migrations/2021_11_10_111117_create_game_sessions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGameSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('game_sessions', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('started_at');
+            $table->dateTime('ended_at');
+            $table->foreignId('profile_id')->nullable()->constrained();
+            $table->integer('points')->default(0);
+            $table->boolean('is_finished')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('game_sessions');
+    }
+}
