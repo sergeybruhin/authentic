48f1abe0ddc7de9b3558fa5d44face3be7269f4c
Sergey Bruhin <sundaycreative@gmail.com>
2021-12-06 13:15:21 +0300

    ... minor

--- app/Models/Collection.php
+++ app/Models/Collection.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Collection extends Model
@@ -16,5 +17,12 @@ class Collection extends Model
     public function collectionable(): MorphTo
     {
         return $this->morphTo();
+    }
+
+    public function users(): BelongsTo
+    {
+        return $this->belongsToMany(
+            User::class
+        ),
     }
 }
